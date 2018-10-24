0d432b90ef03a53dac2c69acf8556e44cbf58cf7
Edward Whelan <vroxing@gmail.com>
2018-10-23 22:50:32 -0400

    plan api usage. stage for fetch

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,8 @@
 // console.log('it works?!');
 
+//Set up fetch for google civic api to look at voter information
+// https://www.googleapis.com/civicinfo/v2/voterinfo?address=3431%20orchard%20circle%20decatur%20ga%2030032&key=APIKEYHERE
+
+//helper functions
+
+//Main function
