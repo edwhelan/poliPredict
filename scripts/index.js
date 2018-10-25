01089af7888ca5647e6f9dfe0e4b6217da47ade0
Edward Whelan <vroxing@gmail.com>
2018-10-24 21:41:58 -0400

    add base api call

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,19 @@
 // console.log('it works?!');
+//GLOBAL VARS
+const voterAddress = `3431%20orchard%20circle%20decatur%20ga%2030032`
 
 //Set up fetch for google civic api to look at voter information
-// https://www.googleapis.com/civicinfo/v2/voterinfo?address=3431%20orchard%20circle%20decatur%20ga%2030032&key=APIKEYHERE
+// https://www.googleapis.com/civicinfo/v2/voterinfo?address=3431%20orchard%20circle%20decatur%20ga%2030032&key=${CIVICS_API}
 
+function getVoterInfo() {
+
+  fetch(`https://www.googleapis.com/civicinfo/v2/voterinfo?address=${voterAddress}&key=${CIVICS_API}`)
+    .then(r => r.json())
+    .then(d => getData(d));
+
+}
 //helper functions
-
+function getData(data) {
+  console.log(data.contests);
+}
 //Main function
